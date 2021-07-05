41f9898b573f2a3293a4ae393585497f4ba29697
abhishek <abhishekmore710@gmail.com>
2021-07-05 19:33:04 +0530

    atcoder and codeforces done

--- src/index.js
+++ src/index.js
@@ -70,3 +70,47 @@ api.get('/codeforces',(req,res)=>{
     )();
 
 })
+
+api.get('/atcoder', (req, res) => {
+    let title;
+    const movie = "https://atcoder.jp/contests/";
+    (
+        async () => {
+            let imdbData = [];
+            const respose = await request({
+                uri: movie,
+                headers: {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                    "Accept-Encoding": "gzip, deflate, br",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Cache-Control": "max-age=0",
+                  },
+                gzip: true
+            })
+            let $ = cheerio.load(respose);
+
+            let i = 1;
+            while (1) {
+                title1 = $(`#contest-table-upcoming > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(2) > a`).text().trim();
+                time1 = $(`#contest-table-upcoming > div > div > table > tbody > tr:nth-child(${i}) > td:nth-child(3)`).text().trim();
+                i++;
+
+                if (!time1)
+                    break;
+                imdbData.push({
+                    title1, time1
+                });
+
+            }
+
+            console.log(imdbData);
+
+            //console.log(req);
+            // res.send(`${title1} ${time1}  = name of contest , ${title2} ${time2}`);
+            //res.send(`${imdbData}`);
+            res.json(imdbData);
+        }
+
+    )();
+
+})
